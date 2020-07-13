edec67f51e7731b9efbaa331fc47dfd71f2097e7
honwon <moheom98@gmail.com>
2020-07-14 01:29:44 +0900

    3버전 출시

--- app/src/main/java/com/honwon/punchpower/ResultActivity.kt
+++ app/src/main/java/com/honwon/punchpower/ResultActivity.kt
@@ -10,6 +10,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.games.Games
 import kotlinx.android.synthetic.main.activity_result.*
+import java.text.NumberFormat
 
 class ResultActivity : AppCompatActivity() {
 
@@ -29,7 +30,12 @@ class ResultActivity : AppCompatActivity() {
 
         title = "너의 전투력은?"
 
-        scoreLabel.text = "${String.format("%.0f", power)} 점"
+        scoreLabel.text = "${NumberFormat.getIntegerInstance().format(String.format("%.0f", power).toInt())} 점"
+        if(power >= 1500000){
+            textView3.text = "오호라 좀 쎈데?"
+        } else{
+            textView3.text = "후훗 가소롭군 더 수련하고 와라!"
+        }
 
         restartButton.setOnClickListener {
             finish()
@@ -66,7 +72,7 @@ class ResultActivity : AppCompatActivity() {
             } else {
                 var message = result.status.statusMessage
                 if(message == null || message.isEmpty()){
-                    message = "로그인 오류났다"
+                    message = "로그인 오류났다.. 이거 안고쳐진다 알아서 해라"
                 }
                 Toast.makeText(applicationContext,message,Toast.LENGTH_LONG).show()
             }
